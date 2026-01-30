230aa3a42c9cb1b54c48eb2c524046bffc66c47d
P1X3L-25 <ndmccoy@mail.mccneb.edu>
2026-01-30 07:13:09 +0000

    Edited one final time to get the Vercel deployment working.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,9 +10,17 @@ const nextConfig: NextConfig = {
         crypto: false,
         worker_threads: false,
         perf_hooks: false,
+        module: false,
+        v8: false,
+        child_process: false,
+        'util/types': false,
       };
     }
     return config;
+  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
